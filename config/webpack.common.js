72937d3bd471264de37a4e6c58e97b104d234acb
Junichi Kato <j5ik2o@gmail.com>
2021-07-29 23:54:51 +0900

    wip

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -13,6 +13,21 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=img/[contenthash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+        ],
+      },
     ],
   },
   resolve: {
