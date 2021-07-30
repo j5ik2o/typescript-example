cf54fdcc1e8fb5a0879874f1b01b6b39cd3f1b50
Junichi Kato <j5ik2o@gmail.com>
2021-07-30 18:21:19 +0900

    wip

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,11 +1,15 @@
 const helpers = require('./helpers')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common.js')
+const webpack = require('webpack')
 
 module.exports = merge(commonConfig, {
   mode: 'development',
   target: 'web',
   devtool: 'cheap-module-source-map',
+  entry: ['react-hot-loader/patch', 'webpack/hot/only-dev-server', helpers.root('src/App.tsx')],
   output: {
     path: helpers.root('dist'),
     filename: '[name].bundle.js',
@@ -21,4 +25,20 @@ module.exports = merge(commonConfig, {
       index: '/',
     },
   },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+        mode: 'write-references',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+  ],
 })
